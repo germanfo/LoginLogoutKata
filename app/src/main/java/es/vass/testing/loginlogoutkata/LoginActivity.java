f62884de070610f5e89b1087d60fafb0808b47e4
German Franco <german.franco@vass.es>
2017-11-15 16:40:24 +0100

    Login View with check button enabled

--- app/src/main/java/es/vass/testing/loginlogoutkata/LoginActivity.java
+++ app/src/main/java/es/vass/testing/loginlogoutkata/LoginActivity.java
@@ -2,12 +2,71 @@ package es.vass.testing.loginlogoutkata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_user;
+    private EditText et_password;
+    private Button bt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+
+        addTextWatcher();
     }
+
+    private void addTextWatcher() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkEnableOrDisableLoginButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        et_user.addTextChangedListener(textWatcher);
+        et_password.addTextChangedListener(textWatcher);
+
+        //initial call
+        checkEnableOrDisableLoginButton();
+    }
+
+    private void checkEnableOrDisableLoginButton() {
+        String loginText = et_user.getText().toString();
+        String passwordText = et_password.getText().toString();
+
+        if (!TextUtils.isEmpty(loginText) && !TextUtils.isEmpty(passwordText)){
+            bt_login.setEnabled(true);
+        }
+        else {
+            bt_login.setEnabled(false);
+        }
+    }
+
+    private void initViews() {
+        et_user = findViewById(R.id.et_login);
+        et_password = findViewById(R.id.et_password);
+        bt_login = findViewById(R.id.bt_login);
+    }
+
+
+
 }
